37fdd28f8863ec167eeca7bb06a953c63ccd2d79
AlistairConOFC <103876424+AlistairConOFC@users.noreply.github.com>
2026-05-24 12:58:34 +1000

    Create app.php

---
+++ config/app.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'site_name' => 'VinylHive',
+    'site_url' => 'http://vinylhive.local',
+    'admin_email' => 'admin@vinylhive.local',
+    'items_per_page' => 24,
+    'currency' => '₽',
+    'debug' => true,
+    'yookassa' => [
+        'shop_id' => 'YOUR_SHOP_ID',
+        'secret_key' => 'YOUR_SECRET_KEY'
+    ],
+    'cdek' => [
+        'account' => 'YOUR_CDEK_ACCOUNT',
+        'secure_password' => 'YOUR_CDEK_PASSWORD'
+    ]
+];
